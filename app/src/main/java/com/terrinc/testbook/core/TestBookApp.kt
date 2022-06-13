95b46525cd286b6d4138118ebe25187524ccbe52
terrinc <terrinc@bk.ru>
2022-06-13 16:38:23 +0300

    HB01 domain layer for books

--- app/src/main/java/com/terrinc/testbook/core/TestBookApp.kt
+++ app/src/main/java/com/terrinc/testbook/core/TestBookApp.kt
@@ -1,6 +1,16 @@
 package com.terrinc.testbook.core
 
 import android.app.Application
+import com.terrinc.testbook.data.BooksRepository
+import com.terrinc.testbook.domain.BaseBooksDataToDomainMapper
+import com.terrinc.testbook.domain.BooksInteractor
 
-class TestBookApp: Application() {
+class TestBookApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val booksRepository: BooksRepository = TODO("merge")
+        val interactor = BooksInteractor.Base(booksRepository, BaseBooksDataToDomainMapper())
+    }
 }
---
+++ app/src/main/java/com/terrinc/testbook/domain/BooksInteractor.kt
@@ -0,0 +1,16 @@
+package com.terrinc.testbook.domain
+
+import com.terrinc.testbook.data.BooksDataToDomainMapper
+import com.terrinc.testbook.data.BooksRepository
+
+interface BooksInteractor {
+
+    suspend fun fetchBooks(): BookDomain
+
+    class Base(
+        private val booksRepository: BooksRepository,
+        private val mapper: BooksDataToDomainMapper,
+    ) : BooksInteractor {
+        override suspend fun fetchBooks() = booksRepository.fetchBooks().map(mapper)
+    }
+}
